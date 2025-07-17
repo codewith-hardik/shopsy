5e0600d3dcffda4b87bd334e632825df7154951e
Hardik Chavda <chavdahardik1729@gmail.com>
2025-07-17 10:15:34 +0530

    Add work

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import Image1 from "../../assets/hero/women.png"
-import Image2 from "../../assets/hero/shopping.png"
-import Image3 from "../../assets/hero/sale.png"
-import Slider from 'react-slick'
+import React from "react";
+import Image1 from "../../assets/hero/women.png";
+import Image2 from "../../assets/hero/shopping.png";
+import Image3 from "../../assets/hero/sale.png";
+import Slider from "react-slick";
 
 const ImageList = [
   {
     id: 1,
     img: Image1,
     title: "Upto 50% off on all Men's Wear",
-    description: "Shop now and get upto 50% off on all men's wear.",
+    description:
+      " Get the best deals on Men's Wear. Upto 50% off on all products.",
   },
   {
     id: 2,
     img: Image2,
-    title: "Upto 30% off on all Women's Wear",
-    description: "Shop now and get upto 30% off on all women's wear.",
+    title: "30% off on all Women's Wear",
+    description:
+      " Get the best deals on Women's Wear. Upto 30% off on all products.",
   },
   {
     id: 3,
     img: Image3,
-    title: "70% off on all Products sale",
-    description: "Shop now and get upto 70% off on all products sale.",
+    title: "70% off on all Products Sale",
+    description:
+      " Get the best deals on all products. Upto 70% off on all products.",
   },
-]
+];
 
-const Hero = () => {
-
+const Hero = ({ handleOrderPopup }) => {
   var settings = {
     dots: false,
-    infinite: true,
     arrows: false,
+    infinite: true,
     speed: 800,
     slidesToScroll: 1,
     autoplay: true,
@@ -38,51 +40,70 @@ const Hero = () => {
     cssEase: "ease-in-out",
     pauseOnHover: false,
     pauseOnFocus: true,
-  }
+  };
 
   return (
-    <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
-      {/* background section */}
-      <div className="bg-primary/40 absolute h-[700px] w-[700px] bg rounded-3xl rotate-45 -z[8] -top-1/2 right-0  "></div>
-
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
+      {/* background pattern */}
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z[8]"></div>
       {/* hero section */}
-      <div className='container pb-8 sm:pb-0 '>
-        <Slider {...settings} >
-          {
-            ImageList.map((data) => {
-              <div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 '>
-                  {/* text content section */}
-                  <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative'>
-                    <h1
-                      data-aos="zoom-out"
-                      data-aos-duration="500"
-                      data-aos-once="true"
-                      className='text-5xl sm:text-6xl lg:text-7xl font-bold'
-                    > {data.title}</h1>
-                    <p data-aos="fade-up" data-aos-duration="500" data-as-delay="100" className='text-sm'>
-                      {data.description}
-                    </p>
-                    <div data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">
-                      <button className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>
-                        Order Now
-                      </button>
-                    </div>
+      <div className="container pb-8 sm:pb-0">
+        <Slider {...settings}>
+          {ImageList.map((data) => (
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* text content section */}
+                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                  <h1
+                    data-aos="zoom-out"
+                    data-aos-duration="500"
+                    data-aos-once="true"
+                    className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                  >
+                    {data.title}
+                  </h1>
+                  <p
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="100"
+                    className="text-sm"
+                  >
+                    {data.description}
+                  </p>
+                  <div
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="300"
+                  >
+                    <button
+                      onClick={handleOrderPopup}
+                      className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
+                    >
+                      Order Now
+                    </button>
                   </div>
-                  {/* image section */}
-                  <div className="order-1 sm:order-2">
-                    <div className="relative z-10" data-aos="zoom-in" cata-aos-once="true">
-                      <img src={data.img} alt="image" className='          ' />
-                    </div>
+                </div>
+                {/* image section */}
+                <div className="order-1 sm:order-2">
+                  <div
+                    data-aos="zoom-in"
+                    data-aos-once="true"
+                    className="relative z-10"
+                  >
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto"
+                    />
                   </div>
                 </div>
               </div>
-            })
-          }
+            </div>
+          ))}
         </Slider>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
+import Img1 from "../../assets/women/women.png"
+import Img2 from "../../assets/women/women2.jpg"
+import Img3 from "../../assets/women/women3.jpg"
+import Img4 from "../../assets/women/women4.jpg"
+import { FaStar } from 'react-icons/fa'
 
 const Products = () => {
+  const ProductData = [
+    {
+      id: 1,
+      img: Img1,
+      title: "Women Ethnic",
+      rating: 5.0,
+      color: "White",
+      aosDelay: "0",
+    },
+    {
+      id: 2,
+      img: Img2,
+      title: "Women western",
+      rating: 4.5,
+      color: "Red",
+      aosDelay: "200",
+    },
+    {
+      id: 3,
+      img: Img3,
+      title: "Goggles",
+      rating: 4.5,
+      color: "Brown",
+      aosDelay: "400",
+    },
+    {
+      id: 4,
+      img: Img4,
+      title: "Printed T-Shirt",
+      rating: 5.0,
+      color: "Yellow",
+      aosDelay: "600",
+    },
+    {
+      id: 5,
+      img: Img2,
+      title: "Fashin T-Shirt",
+      rating: 4.0,
+      color: "Pink",
+      aosDelay: "800",
+    },
+    {
+      id: 5,
+      img: Img2,
+      title: "Fashin T-Shirt",
+      rating: 4.0,
+      color: "Pink",
+      aosDelay: "800",
+    }
+  ]
   return (
-    <div>
-      <h1>Products Component</h1>
+    <div className='mt-14 mb-12'>
+      <div className="container">
+        {/* Header section */}
+        <div className="text-center mb-10 max-w-[600] mx-auto ">
+          <p data-aos="fade-up" className='textsm text-primary '>
+            Top Selling Products for you.
+          </p>
+          <h1 className='text-3xl font-bold' data-aos="fade-up"> Products</h1>
+          <p className='text-sm text-gray-400 ' data-aos='fade-up' > Discover the latest fashion trends and must-have items for women and men.</p>
+        </div>
+        {/* body section */}
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 place-items-center gap-5">
+            {/*  card section */}
+            {
+              ProductData.map((data) => (
+                <div className='space-y-3 ' key={data.id} data-aos='fade-up' data-aos-delay={data.aosDelay}>
+                  <img src={data.img} alt="img" className='h-[220px] w-[150px] object-cover rounded-md' />
+                  <div>
+                    <h3 className='font-semibold' >{data.title}</h3>
+                    <p className='text-sm text-gray-600 '>{data.color} </p>
+                    <div className='flex items-center gap-1'>
+                      <FaStar className='text-yellow-400' />
+                      <span>{data.rating}</span>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+
+          </div>
+          {/* view all buttons */}
+          <div className="flex justify-center">
+            <button className='text-center mt-10 cursor-pointer bg-primary text-white py-1 px-5 rounded-md'> View All Products</button>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
